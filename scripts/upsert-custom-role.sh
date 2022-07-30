d980f17486826f1cba7bc28895dc352cf3a44e83
doddatpivotal <dpfeffer@vmware.com>
2022-07-29 23:21:43 -0400

    Alters method of testing for custom role existance

--- scripts/upsert-custom-role.sh
+++ scripts/upsert-custom-role.sh
@@ -9,12 +9,14 @@ CUSTOM_ROLE_YAML_FILE=$1
 echo "Upserting $CUSTOM_ROLE_YAML_FILE"
 
 ROLE=$(yq e .fullName.name $CUSTOM_ROLE_YAML_FILE)
-MATCHING_ROLES=$(tmc iam role list --name $ROLE -oyaml | yq e .totalcount -)
+MATCHING_ROLES=$(tmc iam role list --name $ROLE -oyaml | yq e .roles - | yq e 'length' -)
 
-if [ $MATCHING_ROLES == 0 ]; then
+if [ "$MATCHING_ROLES" == "0" ]; then
+    echo "Creating role: $ROLE"
     tmc iam role create -f $CUSTOM_ROLE_YAML_FILE
     echo "Custom role created: $ROLE"
 else
+    echo "Updating role: $ROLE"
     tmc iam role update -f $CUSTOM_ROLE_YAML_FILE
     echo "Custom role updated: $ROLE"
 fi
